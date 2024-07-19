48ad4b4e475695813babc3d11191c0dd05cca050
youngho <youngho.dev@gmail.com>
2024-07-19 14:12:34 +0900

    Fix: 소셜로그인 오류 수정

--- backend/src/main/java/com/ajangajang/backend/user/controller/SignUpController.java
+++ backend/src/main/java/com/ajangajang/backend/user/controller/SignUpController.java
@@ -1,11 +1,15 @@
 package com.ajangajang.backend.user.controller;
 
+import com.ajangajang.backend.oauth.model.dto.CustomOAuth2User;
 import com.ajangajang.backend.user.model.dto.SignUpDto;
 import com.ajangajang.backend.user.model.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,41 +17,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 @RequiredArgsConstructor
-public class UserController {
+@Slf4j
+public class SignUpController {
 
     private final UserService userService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpDto signUpDto, BindingResult bindingResult) {
+    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpDto signUpDto, BindingResult bindingResult,
+                                    @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
 
         if (bindingResult.hasErrors()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
         // 유저 이름 가져오기
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        String username = customOAuth2User.getUsername();
 
         if (userService.signUp(signUpDto, username) == null) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PostMapping("/test")
-    public ResponseEntity<?> test(@Valid @RequestBody SignUpDto signUpDto, BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors()) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @GetMapping("/")
-    public String testPage() {
-        return "testPage";
     }
 }
