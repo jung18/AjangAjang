8539ee7690490df940bfb42964de6781e97634af
enghddd <jung1213546@gmail.com>
2024-08-07 02:08:31 +0900

    Feat: 판매글 더미데이터 생성기능 추가

--- backend/src/main/java/com/ajangajang/backend/api/kakaomap/controller/KakaoMapApiController.java
+++ backend/src/main/java/com/ajangajang/backend/api/kakaomap/controller/KakaoMapApiController.java
@@ -3,8 +3,11 @@ package com.ajangajang.backend.api.kakaomap.controller;
 import com.ajangajang.backend.api.kakaomap.model.entity.Regions;
 import com.ajangajang.backend.api.kakaomap.model.service.KakaoApiService;
 import com.ajangajang.backend.api.kakaomap.model.service.RegionDataService;
+import com.ajangajang.backend.api.kakaomap.model.service.TestDataService;
+import com.ajangajang.backend.oauth.model.dto.CustomOAuth2User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +17,13 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-public class KakaoMapApiController { // 초기 지역데이터 설정용
+public class KakaoMapApiController { // 초기 데이터 설정용
 
     private final RegionDataService regionDataService;
     private final KakaoApiService kakaoApiService;
+    private final TestDataService testDataService;
 
-    @GetMapping("/api/kakao/address") // 행정구역 정보 조회
+    @GetMapping("/api/kakao/address") // csv에서 행정구역 정보 조회 -> 지역 테이블 만들기
     public ResponseEntity<?> getRegionData() {
         regionDataService.callApiByCSV();
         return ResponseEntity.ok("ok");
@@ -39,8 +43,15 @@ public class KakaoMapApiController { // 초기 지역데이터 설정용
     }
 
     @GetMapping("/api/elastic/address")
-    public ResponseEntity<?> saveElasticRegions() {
+    public ResponseEntity<?> saveElasticRegions() { // elasticsearch 지역 문서 만들기
         regionDataService.saveRegionsES();
+        return ResponseEntity.ok("ok");
+    }
+
+    @GetMapping("/api/board/test")
+    public ResponseEntity<?> boardTestDataInit(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        String username = customOAuth2User.getUsername();
+        testDataService.testDataInit(username);
         return ResponseEntity.ok("ok");
     }
 
