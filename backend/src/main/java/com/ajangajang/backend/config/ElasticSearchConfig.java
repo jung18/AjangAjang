ede58b06459e3bf55f50f75e82c0721d86946d44
전성권 <jeonsg9904@gmail.com>
2024-08-09 09:30:59 +0900

    Feat : 프로퍼티 수정

--- backend/src/main/java/com/ajangajang/backend/config/ElasticSearchConfig.java
+++ backend/src/main/java/com/ajangajang/backend/config/ElasticSearchConfig.java
@@ -1,29 +1,31 @@
 package com.ajangajang.backend.config;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import javax.net.ssl.SSLContext;
 
-@EnableElasticsearchRepositories
-public class ElasticSearchConfig extends ElasticsearchConfiguration {
+public class RestHighLevelClientTest {
+    public static void main(String[] args) throws Exception {
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", "w40NNJvOQzv61sO3=HAd"));
 
-    @Value("${spring.elasticsearch.uris}")
-    private String host;
-
-    @Value("${spring.elasticsearch.username}")
-    private String username;
-
-    @Value("${spring.elasticsearch.password}")
-    private String password;
-
-    @Override
-    public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(host)
-                .withBasicAuth(username, password)
+        SSLContext sslContext = new SSLContextBuilder()
+                .loadTrustMaterial(null, (x509Certificates, s) -> true)
                 .build();
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost("i11b210.p.ssafy.io", 9200, "https"))
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
+                        .setDefaultCredentialsProvider(credentialsProvider)
+                        .setSSLContext(sslContext)
+                        .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE));
+
     }
 }
