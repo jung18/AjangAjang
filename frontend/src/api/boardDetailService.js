91eef4be3211e9dfa68987252382b18a98c5ac0a
kinterlocked <dusehd0525@naver.com>
2024-08-11 21:03:59 +0900

    주소 수정

--- frontend/src/api/boardDetailService.js
+++ frontend/src/api/boardDetailService.js
@@ -4,11 +4,7 @@ export const fetchBoardDetail = async (id) => {
   try {
     const { accessToken } = useTokenStore.getState();
 
-<<<<<<< HEAD
-    const url = "https://i11b210.p.ssafy.io:8443/api/board/" + id;
-=======
-    const url = "http://localhost:8080/api/board/" + id;
->>>>>>> feature/frontend-chatdesign
+    const url = "https://i11b210.p.ssafy.io:4443/api/board/" + id;
     //거래 유형 별로 해당되는 유형의 게시글만 넘겨주도록 서버 코드 수정 필요
     const response = await fetch(url, {
       method: "GET",
--- frontend/src/api/reviewService.js
+++ frontend/src/api/reviewService.js
@@ -5,11 +5,7 @@ export const fetchReviewList = async () => {
     const { accessToken } = useTokenStore.getState();
 
     //거래 유형 별로 해당되는 유형의 게시글만 넘겨주도록 서버 코드 수정 필요
-<<<<<<< HEAD
-    const response = await fetch("https://i11b210.p.ssafy.io:8443/api/board/all", {
-=======
-    const response = await fetch("http://localhost:8080/api/board/all", {
->>>>>>> feature/frontend-chatdesign
+    const response = await fetch("https://i11b210.p.ssafy.io:4443/api/board/all", {
       method: "POST",
       headers: {
         "Authorization": `${accessToken}`,
