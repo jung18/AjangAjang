0ad58479e38e4f9e5a5e96d1828b19a8a5dcb615
이지수 <book0256@naver.com>
2024-08-13 10:54:32 +0900

    Feat: 아이 목록, 주소 목록 화면

--- frontend/src/pages/myInfo/MyInfo.jsx
+++ frontend/src/pages/myInfo/MyInfo.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+
 import "./MyInfo.css";
+import AddIcon from "../../assets/icons/add.png";
+import DeleteIcon from "../../assets/icons/close.png";
+
 import usePageStore from "../../store/currentPageStore";
+
 import { useNavigate } from "react-router-dom";
 
 function MyInfo() {
@@ -30,6 +35,50 @@ function MyInfo() {
           <button onClick={editBtnClickHandler} type="button">
             수정
           </button>
+        </div>
+        <div className="address">
+          <div className="address-header">
+            <div className="address-title">주소 목록</div>
+            <div className="address-btns">
+              <div className="change-rep">대표변경</div>
+              <img src={AddIcon} alt="icon" className="add-address" />
+            </div>
+          </div>
+          <div className="address-list">
+            <div className="address-item">
+              <div className="address-content">서울시 양천구 목동동로 130</div>
+              <div className="address-item-btns">
+                <div className="address-type-set">범위설정</div>
+                <img alt="icon" src={DeleteIcon} />
+              </div>
+            </div>
+            <div className="address-item">
+              <div className="address-content">서울시 양천구 목동동로 130</div>
+              <div className="address-item-btns">
+                <div className="address-type-set">범위설정</div>
+                <img alt="icon" src={DeleteIcon} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="children">
+          <div className="children-header">
+            <div className="children-title">아이 목록</div>
+            <div className="children-btns">
+              <div className="change-rep">대표변경</div>
+              <img src={AddIcon} alt="icon" className="add-children" />
+            </div>
+          </div>
+          <div className="children-list">
+            <div className="children-item">
+              <div className="children-content">아이1 / 여자 / 2024-08-01</div>
+              <img alt="icon" src={DeleteIcon} />
+            </div>
+            <div className="children-item">
+              <div className="children-content">아이2 / 여자 / 2024-08-01</div>
+              <img alt="icon" src={DeleteIcon} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
