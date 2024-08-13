7d09764f4e05f05b46479c48dbf6383aaf501bf0
이지수 <book0256@naver.com>
2024-08-13 15:31:22 +0900

    Feat: 주소 기능

---
+++ frontend/src/api/myInfoService.js
@@ -0,0 +1,132 @@
+import useTokenStore from "../store/useTokenStore";
+
+export const fetchMyAddressList = async () => {
+  try {
+    const { accessToken } = useTokenStore.getState();
+
+    //거래 유형 별로 해당되는 유형의 게시글만 넘겨주도록 서버 코드 수정 필요
+    const response = await fetch("http://localhost:8080/api/address/my", {
+      method: "GET",
+      headers: {
+        Authorization: `${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching address list", error);
+    throw error;
+  }
+};
+
+export const fetchMyChildrenList = async () => {
+  try {
+    const { accessToken } = useTokenStore.getState();
+
+    //거래 유형 별로 해당되는 유형의 게시글만 넘겨주도록 서버 코드 수정 필요
+    const response = await fetch("http://localhost:8080/api/user/child", {
+      method: "GET",
+      headers: {
+        Authorization: `${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching children list", error);
+    throw error;
+  }
+};
+
+export const updateAddressRep = async (id) => {
+  try {
+    const { accessToken } = useTokenStore.getState();
+
+    //거래 유형 별로 해당되는 유형의 게시글만 넘겨주도록 서버 코드 수정 필요
+    const response = await fetch("http://localhost:8080/api/address/main", {
+      method: "POST",
+      headers: {
+        Authorization: `${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: id,
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching children list", error);
+    throw error;
+  }
+};
+
+export const addAddress = async (address) => {
+  console.log(address);
+  try {
+    const { accessToken } = useTokenStore.getState();
+
+    //거래 유형 별로 해당되는 유형의 게시글만 넘겨주도록 서버 코드 수정 필요
+    const response = await fetch("http://localhost:8080/api/address/name", {
+      method: "POST",
+      headers: {
+        Authorization: `${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ addressName: address }), 
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching children list", error);
+    throw error;
+  }
+};
+
+export const deleteAddress = async (id) => {
+  try {
+    const { accessToken } = useTokenStore.getState();
+
+    //거래 유형 별로 해당되는 유형의 게시글만 넘겨주도록 서버 코드 수정 필요
+    await fetch("http://localhost:8080/api/address/my/" + JSON.stringify(id), {
+      method: "DELETE",
+      headers: {
+        Authorization: `${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+  } catch (error) {
+    console.error("Error fetching children list", error);
+    throw error;
+  }
+};
+
+export const setNearType = async (type) => {
+  console.log(type);
+  try {
+    const { accessToken } = useTokenStore.getState();
+
+    //거래 유형 별로 해당되는 유형의 게시글만 넘겨주도록 서버 코드 수정 필요
+    await fetch("http://localhost:8080/api/address/main/range", {
+      method: "POST",
+      headers: {
+        Authorization: `${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ nearType: type }), 
+    });
+  } catch (error) {
+    console.error("Error fetching children list", error);
+    throw error;
+  }
+}
