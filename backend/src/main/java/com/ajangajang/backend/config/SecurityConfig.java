2d74c6baf9e2ea88f13487105b64abf9a41bc2bd
youngho <youngho.dev@gmail.com>
2024-08-13 17:29:47 +0900

    Chore: security 설정 변경

--- backend/src/main/java/com/ajangajang/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ajangajang/backend/config/SecurityConfig.java
@@ -84,11 +84,11 @@ public class SecurityConfig {
         // 경로별 인가 작업
         httpSecurity
                 .authorizeHttpRequests((auth) -> auth
-//                        .anyRequest().permitAll());
-                        .requestMatchers("/reissue").permitAll()
-                        .requestMatchers("/sign-up", "/api/user/sms/**", "/api/address/name").hasRole("GUEST")
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().hasRole("USER"));
+                        .anyRequest().permitAll());
+//                        .requestMatchers("/reissue").permitAll()
+//                        .requestMatchers("/sign-up", "/api/user/sms/**", "/api/address/name").hasRole("GUEST")
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .anyRequest().hasRole("USER"));
 
         // 세션 stateless 설정
         httpSecurity
