0006d19fbee64fb211f83043966f94be9b4045c9
enghddd <jung1213546@gmail.com>
2024-08-15 01:45:13 +0900

    Refactor: 로그 추가

--- frontend/src/api/locationService.js
+++ frontend/src/api/locationService.js
@@ -4,11 +4,11 @@ import apiClient from "./apiClient";
 export const fetchRoomData = async (id) => { // 추천 위치
     try {
       const response = await apiClient.get("/api/rooms/myRooms/" + id);
+      console.log("fetchRoomData: ", response)
       const data = await response.json();
-      console.log("locationService: ", data)
       return data;
     } catch (error) {
-      console.error("Error fetching recommend locations", error);
+      console.error("Error fetching room data", error);
       throw error;
     }
   };
@@ -16,10 +16,11 @@ export const fetchRoomData = async (id) => { // 추천 위치
   export const fetchUserData = async (id) => { // 추천 위치
     try {
       const response = await apiClient.get("/api/user/" + id);
+      console.log("fetchUserData: ", response)
       const data = await response.json();
       return data;
     } catch (error) {
-      console.error("Error fetching recommend locations", error);
+      console.error("Error fetching user data", error);
       throw error;
     }
 };
