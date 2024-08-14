d9196978f1f80149e7dd78efdfdfe7e5435eca30
이지수 <book0256@naver.com>
2024-08-14 23:36:58 +0900

    Refactor: 상세조회 수정

--- frontend/src/pages/boardDetail/BoardDetail.jsx
+++ frontend/src/pages/boardDetail/BoardDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import axios from "axios";  // axios를 사용하여 API 요청을 처리합니다.
+import axios from "axios"; // axios를 사용하여 API 요청을 처리합니다.
 
 import { deleteMyBoard } from "../../api/boardService";
 import { fetchBoardDetail } from "../../api/boardDetailService";
@@ -24,6 +24,7 @@ function BoardDetail() {
   const [formattedPrice, setFormattedPrice] = useState(0);
   const [isVideoPlaying, setIsVideoPlaying] = useState(false);
   const [videoUrl, setVideoUrl] = useState("");
+  const [formattedDate, setFormattedDate] = useState("");
 
   const user = useUserStore((state) => state.user);
   const navigate = useNavigate();
@@ -40,37 +41,43 @@ function BoardDetail() {
 
   const getCategoryLabel = (category) => {
     switch (category) {
-      case 'DAILY_SUPPLIES':
-        return '일상 용품';
-      case 'BABY_CARRIAGE':
-        return '유모차';
-      case 'FURNITURE':
-        return '아기가구';
-      case 'BABY_CLOTHES':
-        return '아기옷';
-      case 'TOY':
-        return '장난감';
-      case 'CAR_SEAT':
-        return '카시트';
+      case "DAILY_SUPPLIES":
+        return "일상 용품";
+      case "BABY_CARRIAGE":
+        return "유모차";
+      case "FURNITURE":
+        return "아기가구";
+      case "BABY_CLOTHES":
+        return "아기옷";
+      case "TOY":
+        return "장난감";
+      case "CAR_SEAT":
+        return "카시트";
       default:
-        return '기타';
+        return "기타";
+    }
+  };
+
+  const getBoardDetail = async () => {
+    try {
+      const data = await fetchBoardDetail(id); // id를 사용해 fetchBoardDetail 호출
+      setBoardDetail(data);
+      setFormattedPrice(new Intl.NumberFormat("en-US").format(data.price));
+      setIsLiked(data.liked); // 초기 찜 상태를 설정
+      const formattedDate = String(
+        new Date(data.createdAt).toISOString().split("T")[0]
+      );
+      setFormattedDate(formattedDate);
+      console.log(boardDetail.createdAt);
+      console.log(formattedDate);
+    } catch (error) {
+      console.error("Failed to fetch board details:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    const getBoardDetail = async () => {
-      try {
-        const data = await fetchBoardDetail(id); // id를 사용해 fetchBoardDetail 호출
-        setBoardDetail(data);
-        setFormattedPrice(new Intl.NumberFormat("en-US").format(data.price));
-        setIsLiked(data.liked);  // 초기 찜 상태를 설정
-      } catch (error) {
-        console.error("Failed to fetch board details:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     getBoardDetail();
   }, [id]); // boardId가 변경될 때마다 useEffect 재실행
 
@@ -80,12 +87,12 @@ function BoardDetail() {
         // 찜하기 취소 요청
         await apiClient.delete(`api/board/${id}/likes`);
         setIsLiked(false); // 찜 상태 업데이트
-        alert("좋아요를 취소했습니다.")
+        alert("좋아요를 취소했습니다.");
       } else {
         // 찜하기 요청
         await apiClient.post(`api/board/${id}/likes`);
         setIsLiked(true); // 찜 상태 업데이트
-        alert("좋아요를 눌렀습니다.")
+        alert("좋아요를 눌렀습니다.");
       }
     } catch (error) {
       console.error("Failed to toggle like status:", error);
@@ -107,7 +114,9 @@ function BoardDetail() {
 
   const videoBtnClickHandler = () => {
     // VIDEO 타입의 미디어를 찾는다
-    const videoMedia = boardDetail.mediaList.find((media) => media.mediaType === "VIDEO");
+    const videoMedia = boardDetail.mediaList.find(
+      (media) => media.mediaType === "VIDEO"
+    );
 
     const url = videoMedia ? videoMedia.mediaUrl : "";
 
@@ -132,7 +141,7 @@ function BoardDetail() {
           className="like-btn"
           alt="like"
           src={isLiked ? LikeActiveIcon : LikeIcon}
-          onClick={toggleLikeStatus}  // 클릭 시 찜 상태를 토글
+          onClick={toggleLikeStatus} // 클릭 시 찜 상태를 토글
         />
         <img
           className="video-btn"
@@ -143,12 +152,15 @@ function BoardDetail() {
       </div>
       <div className="profile-bar">
         <div className="writer-profile">
-          <img alt="작성자 프로필" src={boardDetail.writer.profileImage || ImageNotFound} />
+          <img
+            alt="작성자 프로필"
+            src={boardDetail.writer.profileImage || ImageNotFound}
+          />
           <div className="writer-info">
             <div className="writer-name">{boardDetail.writer.nickname}</div>
             <div className="other-info">
-              <span className="level">레벨 정보</span>
-              <span>지역</span>
+              <span className="level">{boardDetail.writer.level}</span>
+              <span>{boardDetail.address}</span>
             </div>
           </div>
         </div>
@@ -172,8 +184,10 @@ function BoardDetail() {
           <div className="info-left">
             <div className="post-title">{boardDetail.title}</div>
             <div className="post-other-info">
-              <span className="post-category">{getCategoryLabel(boardDetail.category)}</span>
-              <span>작성 시각</span>
+              <span className="post-category">
+                {getCategoryLabel(boardDetail.category)}
+              </span>
+              <span>{formattedDate}</span>
             </div>
           </div>
           <div className="post-price">{formattedPrice}원</div>
